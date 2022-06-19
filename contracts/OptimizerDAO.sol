dd4c52bb50f9b942c58a47910aecfef8af438e64
Tenzin Rose <tenzinrose@gmail.com>
2022-06-19 16:48:20 -0500

    User can now: join DAO, add tokens (w/ performance / confidence votes), and there is a function available for weighted averages

--- contracts/OptimizerDAO.sol
+++ contracts/OptimizerDAO.sol
@@ -4,11 +4,8 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract OptimizerDAO {
+contract OptimizerDAO is ERC20 {
   // May be able to delete membersTokenCount as tally is taken care of in ERC contract
-  mapping(address => uint) public membersTokenCount;
-  address public LPTokenAddress;
-  uint public tokenReserve;
   uint public treasuryEth;
 
   //
@@ -17,6 +14,7 @@ contract OptimizerDAO {
   // Proposal struct of token, expected performance and confidence level.
   struct Proposal {
     uint date;
+    string[] tokens;
     // Maps Token (i.e 'btc') to array
     mapping(string => uint[]) numOfUserTokens;
     // Maps Token string to array of total token amount
@@ -30,8 +28,7 @@ contract OptimizerDAO {
   Proposal[] public proposals;
 
 
-  constructor(address _LPTokenAddress) {
-    LPTokenAddress = _LPTokenAddress;
+  constructor() ERC20("Optimizer DAO Token", "ODP") {
     // On DAO creation, a vote/proposal is created which automatically creates a new one every x amount of time
     Proposal storage proposal = proposals.push();
     proposal.date = block.timestamp;
@@ -41,88 +38,95 @@ contract OptimizerDAO {
 
   function joinDAO() public payable {
     // What is the minimum buy in for the DAO?
-    require(msg.value > 1 ether);
+    require(msg.value >= 1 ether, "Minimum buy in is 1 ether");
 
     if (treasuryEth == 0) {
 
       // If there is nothing in the treasury, provide liquidity to treasury
       // LP tokens are initially provided on a 1:1 basis
       treasuryEth = msg.value;
-      ERC20(LPTokenAddress).transferFrom(address(this), msg.sender, treasuryEth);
-      tokenReserve = ERC20(LPTokenAddress).totalSupply() - ERC20(LPTokenAddress).balanceOf(address(this));
+      console.log(treasuryEth);
+      // change to _mint
+      _mint(msg.sender, treasuryEth);
 
     } else {
       // DAO members token count is diluted as more members join / add Eth
-      uint proportionOfTokens = (msg.value * tokenReserve) / treasuryEth;
-      tokenReserve += proportionOfTokens;
       treasuryEth += msg.value;
-      ERC20(LPTokenAddress).transferFrom(address(this), msg.sender, proportionOfTokens);
+      uint ethReserve =  treasuryEth - msg.value;
+      uint proportionOfTokens = (msg.value * totalSupply()) / ethReserve;
+      // change to _mint
+      _mint(msg.sender, proportionOfTokens);
     }
   }
 
 
   function leaveDAO() public {
-    uint tokenBalance = ERC20(LPTokenAddress).balanceOf(msg.sender);
+    uint tokenBalance = balanceOf(msg.sender);
     require(tokenBalance > 0);
 
     // User gets back the relative % of the
-    uint ethToWithdraw = (tokenBalance / tokenReserve) * treasuryEth;
-    ERC20(LPTokenAddress).transferFrom(msg.sender, address(this), tokenBalance);
+    uint ethToWithdraw = (tokenBalance / totalSupply()) * treasuryEth;
+    _burn(msg.sender, tokenBalance);
     payable(msg.sender).transfer(ethToWithdraw);
-    tokenReserve -= tokenBalance;
     treasuryEth -= ethToWithdraw;
   }
 
 
-  function submbitVote(string[] memory _token, int[] memory _perfOfToken, uint[] memory _confidenceLevels) public onlyMember {
+  function submbitVote(string[] memory _token, uint[] memory _perfOfToken, uint[] memory _confidenceLevels) public onlyMember {
     // User inputs token they'd like to vote on, the expected performance of token over time period and their confidence level
     // Loop through each token in list and provide a +1 on list
     // If token is in proposal, include in Struct and output average for Performance & confidence levels
     require((_token.length == _perfOfToken.length) && (_perfOfToken.length == _confidenceLevels.length), "Arrays must be the same size");
 
-    uint numberOfVoterTokens = ERC20(LPTokenAddress).balanceOf(msg.sender);
+    uint numberOfVoterTokens = balanceOf(msg.sender);
     for (uint i = 0; i < _token.length; i++) {
       // get each value out of array
+      proposals[proposals.length - 1].tokens.push(_token[i]);
       proposals[proposals.length - 1].userWeightings[_token[i]].push(_perfOfToken[i]);
-      proposals[proposals.length - 1].numOfUserTokens[_token[i]].push(numberOfVoterTokens[i]);
+
+      proposals[proposals.length - 1].numOfUserTokens[_token[i]].push(numberOfVoterTokens);
       proposals[proposals.length - 1].userConfidenceLevel[_token[i]].push(_confidenceLevels[i]);
 
     }
   }
 
+  // Event to emit for Python script to pick up data for model?
 
-  function findTokenWeight(string memory _token) public returns(uint) {
+
+
+  function findTokenWeight() public  {
     uint sumOfLPForToken;
 
     uint numeratorToken;
     uint numeratorConfidence;
 
-    for (uint i = 0; i < proposals[proposals.length - 1].tokenWeightings[_token].length; i++) {
-      sumOfLPForToken += proposals[proposals.length - 1].tokenWeightings[_token][i];
+    for (uint i = 0; i < proposals[proposals.length - 1].tokens.length; i++) {
+      string memory _token = proposals[proposals.length - 1].tokens[i];
+      sumOfLPForToken += proposals[proposals.length - 1].numOfUserTokens[_token][i];
       numeratorToken += proposals[proposals.length - 1].numOfUserTokens[_token][i] * proposals[proposals.length - 1].userWeightings[_token][i];
       numeratorConfidence += proposals[proposals.length - 1].numOfUserTokens[_token][i] * proposals[proposals.length - 1].userConfidenceLevel[_token][i];
+
+      uint weightedAveragePerformance = numeratorToken / sumOfLPForToken;
+      uint weightedAverageConfidence = numeratorConfidence / sumOfLPForToken;
+
+      // This will return a number with 18 decimals, need to divide by 18
+      proposals[proposals.length - 1].proposalFinalPerformance[_token] = weightedAveragePerformance;
+      proposals[proposals.length - 1].proposalFinalConfidence[_token] = weightedAverageConfidence;
     }
-    uint weightedAveragePerformance = numeratorToken / sumOfLPForToken;
-    uint weightedAverageConfidence = numeratorConfidence / sumOfLPForToken;
-    proposals[proposals.length - 1].proposalFinalPerformance[_token] = weightedAveragePerformance;
-    proposals[proposals.length - 1].proposalFinalConfidence[_token] = weightedAverageConfidence;
+
 
     // Update Token weightings mapping
     // initialize tradesOnUniswap function
 
   }
 
-  function rebalance() public {
-    // Function is hit by external source... Chainlink?
-    // Asset weightings is adjusted based on model
-  }
 
   function initiateTradesOnUniswap() public {
 
   }
 
   modifier onlyMember {
-      require(ERC20(LPTokenAddress).balanceOf(msg.sender) > 0);
+      require(balanceOf(msg.sender) > 0);
       _;
    }
 
